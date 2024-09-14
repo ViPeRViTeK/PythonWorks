8ed8e697e85df2ce18f9f3b2860b5646dd1500f3
Viktor Tokarev <Shostik2020@gmail.com>
2024-09-14 20:49:41 +0300

    Update peremennniee.py

--- peremennniee.py
+++ peremennniee.py
@@ -1,5 +1,5 @@
 Urban = 'Python,'
-Work = '12,'
-time_work = '1.5,'
+Work = 12,
+time_work = 1.5,
 time_manage = time_work/Work
 print("Курс:",Urban,"всего задач:",Work,"затрачено часов:",time_work,"среднее время выполнения:",time_manage,'.')
