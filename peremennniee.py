3773c6dec9afd3bfb8206f7075c312a51b080a43
ViPeRViTeK <Shostik2020@gmail.com>
2024-09-14 17:11:11 +0300

    Add files via upload

---
+++ peremennniee.py
@@ -0,0 +1,5 @@
+Urban = 'Python,'
+Work = '12,'
+time_work = '1.5,'
+time_manage = 1.5/12
+print("Курс:",Urban,"всего задач:",Work,"затрачено часов:",time_work,"среднее время выполнения:",time_manage,'.')
