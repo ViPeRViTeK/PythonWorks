4e787dd20d167b72fbaff8c5dbaa2d6b03afd014
Viktor Tokarev <Shostik2020@gmail.com>
2024-09-15 19:51:51 +0300

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -1,8 +1,8 @@
 name = 'Name:Viktor'
 print(name)
 age = 37
-print('Age:',age)
-age = 37+1
+print('Age:', age)
+age = age + 1
 print('New age:', age)
 is_student = 38>37
 print('Is student:',is_student)
